c2444194638941ea3d61deacb57f133d7b9b6a69
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2021-11-12 15:31:51 +0100

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//component file
+import { BrowserRouter as Router } from 'react-router-dom';
+// component file
 import TodoContainer from './FunctionBased/Components/TodoContainer';
-import "./FunctionBased/App.css"
-import { BrowserRouter as Router } from "react-router-dom"
+import './FunctionBased/App.css';
 
 ReactDOM.render(
   // React provides for us the StrictMode to activate checks and logs a warning message at runtime.
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
       <TodoContainer />
-    </Router>  
-  </React.StrictMode>, 
-  document.getElementById("root")
-)
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
